d398cc8a05d361823d2b06f1f8636149672f8997
Naveenkumar123-hash <120244421+Naveenkumar123-hash@users.noreply.github.com>
2023-06-09 13:12:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ const person = {
   age: 80,
 };
 
+let team=players;
+let team1=players.slice(0);
+for(let x on person){
+	let cap1=console.log(x,person[x]);
+}
+}
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
 window.players = players;
